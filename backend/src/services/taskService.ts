9add9fe82e0e7e4b4fe507e05566f2a3188ef201
Ekdvs <ekdvsampath02@gmail.com>
2025-10-05 10:03:48 +0530

    feat: add task service with CRUD operations and update dependencies
    - Added taskService with functions to get recent tasks, create a new task, and mark a task as completed.
    - Updated package.json to include necessary dependencies for Prisma, Express, Jest, and TypeScript.

---
+++ backend/src/services/taskService.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma=new PrismaClient();
+
+//get resenr five tasks
+export const getRecentTasks=async()=>{
+    return prisma.task.findMany({
+        where:{completed:false},
+        orderBy:{createdAt:'desc'},
+        take:5
+    });
+}
+
+//create a new task
+export const createTask=async(title:String,description:String)=>{
+    return prisma.task.create({
+        data:{
+            title,
+            description
+        }
+    });
+}
+
+//markTaskAsCompleted
+export const markTaskDone=async(id:number)=>{
+    const task=await prisma.task.findUnique({
+        where:{id}
+    });
+    if(!task){
+        return null;
+    }
+
+    return prisma.task.update({
+        where:{id},
+        data:{completed:true}
+    })
+}
