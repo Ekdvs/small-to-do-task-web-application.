620a87cbacb2e78d0441bd846704e4db3c899ad5
Ekdvs <ekdvsampath02@gmail.com>
2025-10-06 06:32:57 +0530

    feat: implement task management API with axios integration

---
+++ frontend/frontend/src/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const BASE_URL="http://localhost:4000"
+
+export type Task={
+    id:number,
+    title:string,
+    description:string,
+    completed:boolean
+    createdAt:string
+
+}
+
+export const createTask = async (title:string,description:string)=>{
+    const response =await axios.post(`${BASE_URL}/api/tasks/create-task`,{title,description});
+    return response.data.data as Task;
+}
+
+export const getRecentTasks = async()=>{
+    const response = await axios.get(`${BASE_URL}/api/tasks/recent-tasks`);
+    return response.data.data as Task[];
+}
+
+export const markTaskDone = async(id:number)=>{
+    const response = await axios.patch(`${BASE_URL}/api/tasks//${id}mark-done`);
+    return response.data.data as Task;
+}
+
